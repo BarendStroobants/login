58484105eb86c4c2c18b608af9b977ef3196685c
Baloe <barend.stroobants@gmail.com>
2019-10-24 16:41:58 +0200

    I'm stuck

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
+ini_set('display_errors', "1");
+ini_set('display_startup_errors', "1");
+error_reporting(E_ALL);
+
 require 'connection.php';
 require 'whatIsHappening.php';
 require 'insert.php';
@@ -10,3 +16,25 @@ whatIsHappening();
 
 $con = openConnection();
 
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $connection = openConnection();
+    $firstName = $_POST['first_name'];
+    $lastName = $_POST['last_name'];
+    $username = $_POST['username'];
+    $gender = $_POST['gender'];
+    $linkedin = $_POST['linkedin'];
+    $github = $_POST['github'];
+    $email = $_POST['email'];
+    $preferredLanguage = $_POST['preferred_language'];
+    $avatar = $_POST['avatar'];
+    $video = $_POST['video'];
+    $quote = $_POST['quote'];
+    $quoteAuthor = $_POST['quote_author'];
+
+
+    $sqlCmd = 'INSERT INTO student (first_name, last_name, username, gender, linkedin, github, email, 
+                 preferred_language, avatar, video, quote, quote_author, created_at) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,now())';
+    $connection->prepare($sqlCmd)->execute([$firstName, $lastName, $username, $gender, $linkedin, $github, $email, $preferredLanguage, $avatar,
+        $video, $quote, $quoteAuthor]);
+
+}
